00ed7f7c46b23703ba61bc8ac0cd11a224566ba4
Arko <arka.bhuin@gmail>
2023-01-09 01:57:44 +0530

    fourth commit

--- src/ArraySum.java
+++ src/ArraySum.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ArraySum {
+import java.util.*;
+public class ArraySum {
+    public static int sum(int[] arr) {
+        int t;
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+
+        }
+        return sum;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int  t = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(sum(arr));
+
+
+    }
 }
